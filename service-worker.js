6afb40720b6c92048ade3701686fa27c3c4df27d
matiasmorant <matias.morant.mm@gmail.com>
2026-01-24 18:35:35 -0300

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,25 @@
-const CACHE_NAME = 'chronos-v1';
+const CACHE_NAME = 'chronos-v2';
 const DATA_CACHE_NAME = 'chronos-data-v1';
 
 const CORE_ASSETS = [
   './',
   './index.html',
+  './analytics.js',
+  './duration-picker-modal.js',
+  './series-chart.js',
+  './calendar.js',
+  './group-manager.js',
+  './series-history.js',
+  './chart-utils.js',
+  './groupcard.js',
+  './seriesConfig.js',
+  './chronos-chart.js',
+  './multiselect.js',
+  './db.js',
+  './seriecard.js',
+  './utils.js',
+  './dborm.js',
+  './series-chart-config.js',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
@@ -11,11 +27,14 @@ const CORE_ASSETS = [
 
 const CDN_ASSETS = [
   'https://cdn.tailwindcss.com',
-  'https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js',
-  'https://cdn.jsdelivr.net/npm/chart.js',
-  'https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns',
+  'https://unpkg.com/alpinejs@3.x.x/dist/module.esm.js',
+  // 'https://cdn.jsdelivr.net/npm/chart.js',
+  // 'https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns',
   'https://unpkg.com/papaparse@latest/papaparse.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
+  'https://unpkg.com/tabulator-tables@5.6.1/dist/css/tabulator_modern.min.css',
+  'https://unpkg.com/tabulator-tables@5.6.1/dist/js/tabulator.min.js',
+  'https://cdn.jsdelivr.net/npm/date-fns@4.1.0/+esm'
 ];
 
 // Chronos doesn't use external data files like the example, 
